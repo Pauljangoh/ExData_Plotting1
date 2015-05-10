7745bafdb28219d80270cef4ceec637b6e22d237
Pauljangoh <kellbros@naver.com>
2015-05-10 23:52:03 +0900

    Course Project

---
+++ Plot code.R
@@ -0,0 +1,40 @@
+## I had a hard time to subset the data so I edited the txt file
+
+## read the file
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.string="?")
+
+## adjest the colum of the data
+data$Datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+## Plot 1
+hist(data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+
+## Plot 2 
+plot(data$Datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+## Plot 3
+with(data, {
+  plot(Sub_metering_1 ~ Datetime, type = "l", 
+       ylab = "Global Active Power (kilowatts)", xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+})
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Plot 4
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(data, {
+  plot(Global_active_power ~ Datetime, type = "l", 
+       ylab = "Global Active Power", xlab = "")
+  plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
+  plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
+       xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+  legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+         bty = "n",
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ Datetime, type = "l", 
+       ylab = "Global_rective_power", xlab = "datetime")
+})
